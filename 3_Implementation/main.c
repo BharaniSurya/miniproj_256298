e3ce1853e2d00953ae92a5a5385a560263a1630a
bharani surya s <bbsuryaa.siva@gmail.com>
2021-04-15 23:57:08 +0530

    commit30

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,8 +1,6 @@
 #include "car_operations.h"
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
+void car_menu();
 
 /* Start of the application */
 int main(int argc, char *argv[])
